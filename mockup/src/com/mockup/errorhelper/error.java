023bc36db218e0364b1974d2c735142b4060bc0d
riaka <674302957@qq.com>
2015-01-13 18:42:04 +0800

    新加error包

---
+++ mockup/src/com/mockup/errorhelper/error.java
@@ -0,0 +1,40 @@
+package com.mockup.errorhelper;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class error extends HttpServlet{
+
+	@Override
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		resp.setContentType("textml;charset=utf-8");
+		resp.setCharacterEncoding("UTF-8");
+		PrintWriter out = resp.getWriter();
+		
+		out.println("<html>");
+		out.println("<head>");
+		out.println("	<title>Error</title>");
+		out.println("</head>");
+		out.println("<body>");
+		out.println("	<h2 align=\"center\">Error</h2>");
+		out.println("	<hr>");
+		out.println("	System Error," + req.getAttribute("errormessage") + "!");
+		out.println("</body>");
+		out.println("</html>");
+		
+		out.close();
+	}
+
+	@Override
+	public void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		
+	}
+	
+}
