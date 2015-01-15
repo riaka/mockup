f78d763101421dc838a157efd4d9e70fbde94b3e
一起走过的光辉岁月 <@192.168.1.149>
2015-01-15 11:23:07 +0800

    loginvalidate.js 完成

--- mockup/web/js/loginvalidate.js
+++ mockup/web/js/loginvalidate.js
@@ -1,37 +1,39 @@
-﻿function loginvalidate(form){
-	var userName=form.userName;
+﻿function loginvalidate(){
 	var vil=true;
 	if(userName.value.length==0)
 	{
-		document.getElementById("validateuser").innerHTML="<b>用户名不能为空</b>";
 		userName.focus();
+		p1.innerHTML="用户名不能为空";		
 		vil=false;
 	}
 	else if(userName.value.length>8){
-		document.getElementById("validateuser").innerHTML="<b>用户名的长度不能超过8位</b>";
+		
 		userName.focus();
+		p1.innerHTML="用户名的长度不能超过8位";
 		vil=false;
 	}else
 	{
-		//在用户再次提交表单时，如果合法，取消前一次的验证不合法消息
-		document.getElementById("validateuser").innerHTML="";
+		
+		p1.innerHTML="";
 	}
+
 	//验证密码输入是否合法
-	var password=form.password;
+	 
 	if(password.value.length==0)
 	{
-		document.getElementById("validatepassword").innerHTML="<b>密码不能为空</b>";
+		p2.innerHTML="密码不能为空";
 		password.focus();
 		vil=false;
 	}
-	else if(password.value.length<6||password.value.length>8)
+	else if(password.value.length<4||password.value.length>8)
 	{
-		document.getElementById("validatepassword").innerHTML="<b>密码的长度应该介于6到8位之间</b>";
+		p2.innerHTML="密码的长度应该介于4到8位之间";
 		password.focus();
 		vil=false;
 	}else
 	{
-		document.getElementById("validatepassword").innerHTML="";
+		p2.innerHTML="";
 	}
+	 
 	return vil;
 }
